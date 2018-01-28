474f05f68ef13d91ef085a8f5e8d4ca857caa4fd
tjh28 <tjh2822@g.rit.edu>
2018-01-28 13:42:18 -0500

    BME280

---
+++ BME280/bme280/bme280.ino
@@ -0,0 +1,66 @@
+/// RIT SPEX
+/// IREC - Hyperion
+/// Thomas Hall
+/// Amber Dubill
+/// BME 280 Sensor Code
+
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+#include <Adafruit_BME280.h>
+
+/// Sea Level Temp and Pressure for Launch Location
+/// Data is Pressure at Sea Level in hPa
+#define SEALEVELPRESSURE_HPA (1023.23)
+
+Adafruit_BME280 bme; // I2C
+
+unsigned long delayTime;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println(F("BME280 I2C"));
+
+  bool status;
+    
+  // Check status
+  status = bme.begin();  
+  if (!status) {
+      Serial.println("Could not find a valid BME280 sensor, check wiring!");
+      while (1);
+  }
+    
+  Serial.println("-- Test --");
+  delayTime = 1000;
+
+  Serial.println();
+
+  delay(100); // let sensor boot up
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  printValues();
+  delay(delayTime);
+}
+
+void printValues() {
+    Serial.print("Temperature = ");
+    Serial.print(bme.readTemperature());
+    Serial.println(" *C");
+
+    Serial.print("Pressure = ");
+
+    Serial.print(bme.readPressure() / 100.0F);
+    Serial.println(" hPa");
+
+    Serial.print("Approx. Altitude = ");
+    Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
+    Serial.println(" m");
+
+    Serial.print("Humidity = ");
+    Serial.print(bme.readHumidity());
+    Serial.println(" %");
+
+    Serial.println();
+}
