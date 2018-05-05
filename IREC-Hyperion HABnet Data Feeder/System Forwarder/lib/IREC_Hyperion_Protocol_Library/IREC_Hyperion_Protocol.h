8e85f40281c9cd1c406109f1f7dd1a960c388859
Dylan Wagner <vstrideultimate@gmail.com>
2018-05-04 22:15:38 -0400

    Added string buliding code for the LSM9DS1 and BME280

---
+++ IREC-Hyperion HABnet Data Feeder/System Forwarder/lib/IREC_Hyperion_Protocol_Library/IREC_Hyperion_Protocol.h
@@ -0,0 +1,356 @@
+/**
+ * File: IREC-Hyperion_Protocol.h
+ * Organization: RIT Space Exploration
+ * Description:
+ *      Protocol to be used on the Hyperion payload.
+ */
+
+#ifndef IREC_HYPERION_PROTOCOL_LIBRARY_H
+#define IREC_HYPERION_PROTOCOL_LIBRARY_H
+
+#define BYTE_MASK 255
+#define NIBBLE_MASK 15
+
+#include <stdint.h>
+
+// Size in Bytes
+#define HEADER_SIZE 3
+#define LSM9DS1_FRAME_SIZE 36
+#define BME280_FRAME_SIZE 16
+#define CCS811_FRAME_SIZE 4
+#define LIS331_FRAME_SIZE 12
+#define PFSL_FRAME_SIZE 4
+#define OREN_FRAME_SIZE 3
+
+/////////////////////
+/// Data Structs ///
+///////////////////
+
+/**
+ * Unpacked header
+ */
+typedef struct unpk_header{
+    uint8_t data_type;
+    uint8_t flags;
+    uint16_t time;
+} unpk_header;
+
+/**
+ * Data struct for the LSM8DS1 data frame
+ */
+typedef struct LSM9DS1_Data{
+    int32_t ax, ay, az;
+    int32_t gx, gy, gz;
+    int32_t mx, my, mz;
+} LSM9DS1_Data;
+
+/**
+ * Data struct for the BME280 data frame
+ */
+typedef struct BME280_Data{
+    int32_t temperature;
+    int32_t pressure;
+    int32_t humidity;
+    int32_t altitude;
+} BME280_Data;
+
+/**
+ * Data struct for the CSS811 data frame
+ */
+typedef struct CCS811_Data{
+    int16_t co2;
+    int16_t TVOC;
+} CCS811_Data;
+
+/**
+ * Data struct for the LIS311 data frame
+ */
+typedef struct LIS311_Data{
+    int32_t ax, ay, az;
+} LIS311_Data;
+
+/**
+ * Data struct for the PFSL data frame
+ */
+typedef struct PFSL_Data{
+    int32_t alt;
+} PFSL_Data;
+
+/**
+ * Packet struct containing LSM9DS1 data and header.
+ */
+typedef struct LSM9DS1_Packet{
+    unpk_header header;
+    LSM9DS1_Data data;
+} LSM9DS1_Packet;
+
+/**
+ * Packet struct containing BME280 data and header.
+ */
+typedef struct BME280_Packet{
+    unpk_header header;
+    BME280_Data data;
+} BME280_Packet;
+
+/**
+ * Packet struct containing CCS811 data and header.
+ */
+typedef struct CCS811_Packet{
+    unpk_header header;
+    CCS811_Data data;
+} CCS811_Packet;
+
+/**
+ * Packet struct containing LIS311 data and header.
+ */
+typedef struct LIS311_Packet{
+    unpk_header header;
+    LIS311_Data data;
+} LIS311_Packet;
+
+/**
+ * Packet struct containing PFSL data and header.
+ */
+typedef struct PFSL_Packet{
+    unpk_header header;
+    PFSL_Data data;
+} PFSL_Packet;
+
+/**
+ * Data Type Enums
+ */
+enum DataFrameType{
+    NULLDATAt,
+    LSM9DS1t,
+    BME280t,
+    CCS811t,
+    LIS331t,
+    PFSLt,
+    Orent,
+    CMMNDt
+};
+
+// TODO add unpacking functionality
+// TODO finish packing functions for all data groupss
+
+/**
+ * Main class for IREC-HYPERION_PROTOCOL
+ */
+class IRECHYPERP {
+public:
+
+    /**
+     * Determines the type of data contained in buff
+     * @param buff
+     *      Packet data
+     * @return
+     *      A type corresponding to header data
+     */
+    static int typeofData(const uint8_t buff[]){
+        return buff[0] >> 4;
+    }
+
+    /**
+     * Main packer function for the LSMDS1 data frame.
+     * @param flags
+     *      Header flags
+     * @param time
+     *      Time in seconds
+     * @param ax
+     *      Acceleration value on the X axis
+     * @param ay
+     *      Acceleration value on the Y axis
+     * @param az
+     *      Acceleration value on the Z axis
+     * @param gx
+     *      Gyroscope value on the X axis
+     * @param gy
+     *      Gyroscope value on the Y axis
+     * @param gz
+     *      Gyroscope value on the Z axis
+     * @param mx
+     *      Magnetic value on the X axis
+     * @param my
+     *      Magnetic value on the Y axis
+     * @param mz
+     *      Magnetic value on the Z axis
+     * @return
+     *      A fully complete data frame with header
+     */
+    static void createLSM9DS1Frame(uint8_t buff[], char *flags, uint16_t time,
+                       int32_t ax, int32_t ay, int32_t az,
+                       int32_t gx, int32_t gy, int32_t gz,
+                       int32_t mx, int32_t my, int32_t mz);
+
+    /**
+     * Main packer function for the BME280 data frame
+     * @param flags
+     *      Header flags
+     * @param time
+     *      Time in seconds (Epoch)
+     * @param temp
+     *      Temperature in C
+     * @param humidity
+     *      Humidity in percentage
+     * @param pressure
+     *      Pressure
+     * @param altitude
+     *      Altitude in m
+     * @return
+     *      A fully complete data frame with header
+     */
+    static void createBME280Frame(uint8_t buff[], char flags[], uint16_t time,
+                                   int32_t temp, int32_t humidity, int32_t pressure, int32_t altitude);
+
+    /**
+     * Main packer function for the CCS811 data frame.
+     * @param flags
+     *      Header flags
+     * @param time
+     *      Time in seconds (Epoch)
+     * @param co2
+     *      CO2 in ppm
+     * @param TVOC
+     *      TVOC in ppm
+     * @return
+     *      A fully complete data frame with header
+     */
+    static void createCCS811Frame(uint8_t buff[], char flags[], uint16_t time,
+                                   int16_t co2, int16_t TVOC);
+    /**
+     * Main packer function for the LIS311 data frame.
+     * @param flags
+     *      Header flags
+     * @param time
+     *      Time in seconds (Epoch)
+     * @param ax
+     *      Acceleration value on the X axis
+     * @param ay
+     *      Acceleration value on the Y axis
+     * @param az
+     *      Acceleration value on the Z axis
+     * @return
+     *      A fully complete data frame with header
+     */
+    static void createLIS311Frame(uint8_t buff[], char flags[], uint16_t time,
+                                   int32_t ax, int32_t ay, int32_t az);
+
+    /**
+     * Main packer function for the PFSL data frame
+     * @param buff
+     * @param flags
+     * @param time
+     * @param altitude
+     */
+    static void createPFSLFrame(uint8_t buff[], char flags[], uint16_t time, int32_t altitude);
+
+    /**
+     * Unpack function for the LSM9DS1 data frame
+     * @param buff
+     *      A array containing a packed LSM9DS1 packet
+     * @return
+     *      A LSM9DS1 packet
+     */
+    static LSM9DS1_Packet unpack_LSM9DS1(const uint8_t buff[]);
+
+    /**
+     * Unpack function for the BME280 packet
+     * @param buff
+     *      A array containing a packed BME280 packet
+     * @return
+     *      A BME280 packet
+     */
+    static BME280_Packet unpack_BME280(const uint8_t buff[]);
+
+    /**
+     * Unpack function for the CCS811 packet
+     * @param buff
+     *      A array containing a packed CCS811 packet
+     * @return
+     *      A CCS811 packet
+     */
+    static CCS811_Packet unpack_CCS811(const uint8_t buff[]);
+
+    /**
+     * Unpack function for the LIS311 packet
+     * @param buff
+     *      A array containing a packed LIS311 packet
+     * @return
+     *      A LIS311 packet
+     */
+    static LIS311_Packet unpack_LIS311(const uint8_t buff[]);
+
+    /**
+     * Unpack function for the PFSL packet.
+     * @param buff
+     * @return
+     */
+    static PFSL_Packet unpack_PFSL(const uint8_t buff[]);
+
+private:
+    /**
+     * Creates a header using the provided values below.
+     * @param type
+     *      Type of data frame
+     * @param flags
+     *      Flags passes as an array
+     * @param time
+     *      Time in seconds
+     * @return
+     *      A null terminated string
+     */
+    static void createHeader(uint8_t buff[], DataFrameType type, const char *flags, uint16_t time);
+
+    /**
+     * Unpacks function for a hyperion header.
+     * @param buff
+     *      Array of header data values
+     * @return
+     *      A packed header struct
+     */
+    static unpk_header unpack_header(const uint8_t buff[]);
+
+    /**
+     * Unpack function for the LSM9DS1 data segment
+     * @param buff
+     *      Array of LSM9DS1 data values
+     * @return
+     *      A packed LSM9DS1 Data struct
+     */
+    static LSM9DS1_Data unpack_LSM9DS1_Data(const uint8_t buff[]);
+
+    /**
+     * Unpack function for the BME280 data segment
+     * @param buff
+     *      Array of BME280 data values
+     * @return
+     *      A packed BME280 Data struct
+     */
+    static BME280_Data unpack_BME280_Data(const uint8_t buff[]);
+
+    /**
+     * Unpack function for the CCS811 data segment
+     * @param buff
+     *      Array of CCS811 data values
+     * @return
+     *      A packed CCS811 Data struct
+     */
+    static CCS811_Data unpack_CCS811_Data(const uint8_t buff[]);
+
+    /**
+     * Unpack function for the LIS311 data segment
+     * @param buff
+     *      Array of LIS311 data values
+     * @return
+     *      A packed CCS811 Data struct
+     */
+    static LIS311_Data unpack_LIS311_Data(const uint8_t buff[]);
+
+    /**
+     * Unpack function for the PFSL data segment
+     * @param buff
+     * @return
+     */
+    static PFSL_Data unpack_PFSL_Data(const uint8_t buff[]);
+};
+#endif
--- IREC-Hyperion HABnet Data Feeder/System Forwarder/src/SysFwdr.cpp
+++ IREC-Hyperion HABnet Data Feeder/System Forwarder/src/SysFwdr.cpp
@@ -8,22 +8,179 @@
  */
 
 #include <SPI.h>
+#include <IREC_Hyperion_Protocol.h>
+#include <RH_RF95.h>
 
 #define BAUD_RATE 115200
+#define INITAL_STR_SIZE 200
+
+// SPI pins
+#define RFM95_RST     6   // "A"
+#define RFM95_CS      5   // "B"
+#define RFM95_INT     4    // "C"
+
+#define RF95_FREQ 915.0
+
+RH_RF95 rf95(RFM95_CS, RFM95_INT);
+
+/**
+ * Recieve incomming messages then handle them according
+ * to which data type they contain.
+ */
+void handle_incomming_messages(){
+	//TODO
+}
+
+/**
+ * Convert int32_t into a float
+ */
+float convert_int32_float(int32_t inputvalue) {
+	float f;
+	memcpy(&f, &inputvalue, sizeof(f));
+	return f;
+}
+
+/**
+ * Create the header section of the data string to be sent over
+ * a Serial connection.
+ * Args:
+ * 		buff: the source string
+ * 		hdr: the header struct
+ * Format:
+ * 		[DataType],[F1],[F2],[F3],[F4],[Time]
+ * Return:
+ * 		The length of the compiled string
+ */
+int write_header(char* buff, unpk_header hdr){
+
+	char temp[10] = {'\0'};
+	
+	itoa(hdr.data_type, temp, 10);
+	strcat(buff, temp);
+
+	for(int i = 0; i < 4; i++){
+		strcat(buff, ",");
+		if((hdr.flags >> 3-i) & 1){
+			strcat(buff, "1");
+		} else {
+			strcat(buff, "0");
+		}
+	}
+
+	strcat(buff, ",");
+	itoa(hdr.time, temp, 10);
+	strcat(buff, temp);
+
+	return strlen(buff);
+}
+
+/**
+ * Individual csv converter from LSM9DS1 packet to data string
+ * Args:
+ * 		buff: the source string
+ * 		data: raw data struct
+ * Format:
+ * 		[Header],[ax],...,[gx],...,[mx]
+ * Returns:
+ * 		The length of the string constructed
+ */
+int CtCSV_LSM9DS1(char* buff, LSM9DS1_Packet data){
+	
+	LSM9DS1_Data data_frame = data.data;
+
+	write_header(buff, data.header);
+
+	int32_t temp_arry[9] = {data_frame.ax, data_frame.ay, data_frame.az,
+							data_frame.gx, data_frame.gy, data_frame.gz,
+							data_frame.mx, data_frame.my, data_frame.mz};
+	
+	char temp[10] = {'\0'};
+
+	for(int i = 0; i < 10; i++){
+		strcat(buff, ",");
+		float res = convert_int32_float(temp_arry[i]);
+		dtostrf(res, 1, 3, temp);
+		strcat(buff, temp);
+	}
+
+	return strlen(buff);
+}
+
+/**
+ * Construct a string to be sent over serial for the BME280
+ * Args:
+ * 		buff: the string to be constructed
+ * 		data: raw data structure
+ * Format:
+ * 		[Header],[temp],[press],[hum],[alt]
+ * Return:
+ * 		The size of the constructed string
+ */
+int CtCSV_BME280(char* buff, BME280_Packet data){
+
+	BME280_Data data_frame = data.data;
+
+	write_header(buff, data.header);
+
+	int32_t temp_arry[4] = {data_frame.temperature, data_frame.pressure,
+							data_frame.humidity, data_frame.altitude};
+	
+	char temp[10] = {'\0'};
+
+	for(int i = 0; i < 4; i++){
+		strcat(buff, ",");
+		float res = convert_int32_float(temp_arry[i]);
+		dtostrf(res, 1, 3, temp);
+		strcat(buff, temp);
+	}
+
+	return strlen(buff);
+}
+
+int CtCSV_CCS811(char* buff, CCS811_Packet data);
+int CtCSV_LIS331(char* buff, LIS311_Packet data);
+int CtCSV_PFSL(char* buff, PFSL_Packet data);
 
 /**
  * Grabs source from transmission, unpacks data then 
  * converts packed data a comma seperated string.
  * CSV format: 
- * [type],[flag1(1,0)][flag2(1,0)][flag3(1,0)][flag4(1,0)],[time(seconds)],[data],[data]...
+ * [type],[flag1(1,0)],[flag2(1,0)],[flag3(1,0)],[flag4(1,0)],[time(seconds)],[data],[data]...
  * Arguments
  * 		buff, the buffer which the csv string will be placed.
  * 		source, the raw bytes from the payload.
  * Return
  * 		The number of bytes within the buff
  */
-int convert_to_csv(char* buff, const char* source){
+int convert_to_csv(char* buff, const uint8_t* source){
 
+	switch(IRECHYPERP::typeofData(source)){
+		case NULLDATAt:
+		{
+			//No data
+		}
+		break;
+		case LSM9DS1t:
+		{
+			LSM9DS1_Packet packet = IRECHYPERP::unpack_LSM9DS1(source);
+			CtCSV_LSM9DS1(buff, packet);
+		}
+		break;
+		case BME280t:
+		{
+			BME280_Packet packet = IRECHYPERP::unpack_BME280(source);
+			CtCSV_BME280(buff, packet);
+		}
+		break;
+		case CCS811t:
+		break;
+		case LIS331t:
+		break;
+		case PFSLt:
+		break;
+		case Orent:
+		break;
+	}
 }
 
 /**
@@ -31,22 +188,49 @@ int convert_to_csv(char* buff, const char* source){
  */
 void init_LoRa(){
 
+  pinMode(RFM95_RST, OUTPUT);
+  digitalWrite(RFM95_RST, HIGH);
+  
+  // manual reset
+  digitalWrite(RFM95_RST, LOW);
+  delay(10);
+  digitalWrite(RFM95_RST, HIGH);
+  delay(10);
+
+  if (!rf95.init()) {
+	  return 1;
+  }
+
+  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
+  if (!rf95.setFrequency(RF95_FREQ)) {
+	  return 1;
+  }
+
+  // maximum power
+  rf95.setTxPower(23, false);
+
+  return 0;
 }
 
 /**
  * Main initilize function for the SysFwdr program.
  */
-void init(){
+void init_sys(){
+	// Place startup code here
+	Serial.begin(BAUD_RATE);
+	// Pin mode for led
+	pinMode(LED_BUILTIN, OUTPUT);
+	digitalWrite(LED_BUILTIN, LOW);
+
 	init_LoRa();
 }
 
 int main(){
-	// Place startup code here
-	Serial.begin(BAUD_RATE);
 
-	init();
+	init_sys();
 
 	while(true){
 		// Place loop code here
+		handle_incomming_messages();
 	}
 }
