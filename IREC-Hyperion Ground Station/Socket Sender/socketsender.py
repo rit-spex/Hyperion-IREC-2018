bc7950201e6c7def7f5785cbb7329730a173bb01
Alec Herbert <herbert.alec@gmail.com>
2018-05-29 04:53:54 -0400

    added another (shorter) flag for each argument

--- IREC-Hyperion Ground Station/Socket Sender/socketsender.py
+++ IREC-Hyperion Ground Station/Socket Sender/socketsender.py
@@ -12,10 +12,10 @@ def display_ports():
 
 def arg_handler():
     arg_parser = argparse.ArgumentParser(description='Hyperion Ground Station Software - relays data to a HABnet instance')
-    arg_parser.add_argument('--host', dest='hostname', help='HABnet instance hostname', default='127.0.0.1', nargs='?', type=str)
-    arg_parser.add_argument('--port', dest='port', help='HABnet instance port', default=3000, nargs='?', type=int)
-    arg_parser.add_argument('--serial', dest='serial_in', help='Serial input port', default='COM3', nargs='?', type=str)
-    arg_parser.add_argument('--name', dest='name', help='HABnet socket name', default='testSocket', nargs='?', type=str)
+    arg_parser.add_argument('-H', '--host', dest='hostname', help='HABnet instance hostname', default='127.0.0.1', nargs='?', type=str)
+    arg_parser.add_argument('-P', '--port', dest='port', help='HABnet instance port', default=3000, nargs='?', type=int)
+    arg_parser.add_argument('-S', '--serial', dest='serial_in', help='Serial input port', default='COM3', nargs='?', type=str)
+    arg_parser.add_argument('-N', '--name', dest='name', help='HABnet socket name', default='testSocket', nargs='?', type=str)
     return arg_parser.parse_args()
 
 def main():
