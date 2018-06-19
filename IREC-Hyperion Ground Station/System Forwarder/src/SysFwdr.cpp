e5e3e6c5539350813c45c012e00de2e151f9b8a9
Dylan Wagner <vstrideultimate@gmail.com>
2018-06-18 22:36:49 -0400

    Fixed conversion alt error

--- IREC-Hyperion Ground Station/System Forwarder/src/SysFwdr.cpp
+++ IREC-Hyperion Ground Station/System Forwarder/src/SysFwdr.cpp
@@ -217,7 +217,8 @@ int CtCSV_PFSL(char* buff, PFSL_Packet data){
 	char temp[10] = {'\0'};
 
 	strcat(buff, ",");
-	dtostrf(data_frame.alt, temp, 10);
+	float res = convert_int32_float(data_frame.alt);
+	dtostrf(res, 1, 3, temp);
 	strcat(buff, temp);
 
 	return strlen(buff);
